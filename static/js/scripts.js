b21213d417d1ce201342f7ee68fa7d50e359d53c
Michal Kalinowski <subaru.kali@gmail.com>
2019-03-07 12:52:11 +0100

    tests

---
+++ static/js/scripts.js
@@ -0,0 +1,43 @@
+let planetId = 1;
+const swapiRequest = new XMLHttpRequest();
+swapiRequest.open('GET', 'https://swapi.co/api/planets/?page=1');
+swapiRequest.onload = function () {
+	let swapiData = JSON.parse(swapiRequest.responseText);
+	// console.log(swapiData);
+	// console.log(swapiData.results[0].name);
+}
+
+swapiRequest.send();
+window.onload = createEmptyTable;
+
+
+function createEmptyTable() {
+	const tableBody = document.querySelector('#table-body');
+
+	for (let ii = 0; ii < 10; ii++) {
+		let rowId = 'row' + ii;
+		const tableRow = document.createElement('tr');
+		tableRow.setAttribute('id', rowId);
+		tableBody.appendChild(tableRow);
+
+		for (let i = 0; i < 8; i++) {
+			const tableCell = document.createElement('td');
+			tableRow.appendChild(tableCell);
+		}
+	}
+	fillTable();
+}
+
+
+function fillTable() {
+	// const tableBody = document.querySelector('#table-body');
+	// let cols = document.querySelector('#row0').getElementsByTagName('td');
+	// console.log(cols);
+	//
+	// for (let col in cols) {
+	// 	console.log("col", col);
+	// }
+
+
+
+}
