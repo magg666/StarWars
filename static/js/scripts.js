f8002514b53bd2eefc37a76203d938b8df0b4c69
Michal Kalinowski <subaru.kali@gmail.com>
2019-03-08 18:33:51 +0100

    vote buttons

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -7,24 +7,38 @@ swapiRequest.onload = function () {
 	// console.log(swapiData.results[0].diameter);
 	createEmptyTable(swapiData);
 	return swapiData;
-}
+};
 
 swapiRequest.send();
 
+function countVote() {
+	console.log('vote-test');
+}
 
 function createEmptyTable(data) {
 	const tableBody = document.querySelector('#table-body');
+	const nbOfColumns = 8;
 
-	for (let ii = 0; ii < 10; ii++) {
+
+	for (let ii = 0; ii < data.results.length; ii++) {
 		let rowId = 'row' + ii;
 		const tableRow = document.createElement('tr');
 		tableRow.classList.add('row-class');
 		tableRow.setAttribute('id', rowId);
 		tableBody.appendChild(tableRow);
+		console.log(tableRow);
 
-		for (let i = 0; i < 8; i++) {
+		for (let i = 0; i < nbOfColumns; i++) {
 			const tableCell = document.createElement('td');
 			tableRow.appendChild(tableCell);
+
+			if (i > 6) {
+				const voteButton = document.createElement('button');
+				voteButton.classList.add('voteButton');
+				voteButton.setAttribute('id', "vote" + ii);
+				voteButton.addEventListener('click', () => {countVote(); });
+				tableCell.appendChild(voteButton);
+			}
 		}
 	}
 	fillTable(data);
@@ -32,31 +46,31 @@ function createEmptyTable(data) {
 
 
 function fillTable(data) {
-	const tableBody = document.querySelector('#table-body');
 	let rows = document.querySelectorAll('.row-class');
 
 
-	let factor;
+	let requiredPlanetData;
 
 
 	for (let j = 0; j < rows.length; j++) {
 		let row = rows[j];
-		factor = {
+		requiredPlanetData = {
 			planet: [data.results[j].name,
 			         data.results[j].diameter,
 			         data.results[j].climate,
 			         data.results[j].terrain,
 			         data.results[j].surface_water,
 			         data.results[j].population,
-			         data.results[j].residents.length]
+			         data.results[j].residents.length
+			]
 		};
 		// console.log("oneRow", row);
 		// console.log("allRows", rows);
 
 		let cells = row.getElementsByTagName('td');
-		for (let i = 0; i < cells.length; i++) {
+		for (let i = 0; i < requiredPlanetData.planet.length; i++) {
 
-			cells[i].textContent = factor.planet[i];
+			cells[i].textContent = requiredPlanetData.planet[i];
 		}
 	}
 
