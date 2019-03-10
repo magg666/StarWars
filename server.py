971a02b6c1f67ec68df4631019a95554fe29b342
magg666 <mw23127@gmail.com>
2019-03-10 19:27:20 +0100

    added login/logout/registration options

--- server.py
+++ server.py
@@ -1,12 +1,69 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session, url_for, jsonify
+from functools import wraps
+
+import user as user
+
 
 app = Flask(__name__)
+app.secret_key = 'uuu'
+
+
+# wrapper to verified if user is not logged
+def login_forbidden(func):
+    @wraps(func)
+    def wrap(*args, **kwargs):
+        if 'username' not in session:
+            return func(*args, **kwargs)
+        else:
+            return jsonify({'error': 'You are already logged!'})
+
+    return wrap
 
 
 @app.route('/')
 def index():
-    return render_template('main.html')
+    return render_template('planet_table.html')
 
+
+@app.route('/login', methods=['POST'])
+@login_forbidden
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    if user.is_exist_already(username) and user.verify_password(username, password):
+        session['username'] = username
+        return jsonify({'username': username})
+
+    else:
+        return jsonify({'error': 'Wrong username or password!'})
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/registration', methods=['POST'])
+@login_forbidden
+def registration():
+    username = request.form['login']
+    first_password = request.form['pass']
+    validation_password = request.form['secondpass']
+
+    if not user.is_all_data_validate(username, first_password, validation_password):
+        return jsonify({'error': 'Please provide all data'})
+
+    if user.is_exist_already(username):
+        return jsonify({'error': 'This user already exists'})
+
+    if not user.is_passwords_equal(first_password, validation_password):
+        return jsonify({'error': 'Passwords must be equals'})
+
+    else:
+        user.registration(username, first_password)
+        return jsonify({'username': username})
 
 
 if __name__ == '__main__':
