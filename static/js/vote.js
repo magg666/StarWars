f584ec0aa6b068cdc1c6967a37c013e72e8aeac7
magg666 <mw23127@gmail.com>
2019-03-15 23:06:16 +0100

    added vote js

---
+++ static/js/vote.js
@@ -0,0 +1,43 @@
+export {prepareVoteButton, sendPlanetNameToServer}
+
+
+
+
+
+
+function prepareVoteButton() {
+    let voteButtons = document.querySelectorAll('.vote-button');
+    let planetLinks = document.querySelectorAll('.row-class');
+    for (let i = 0; i < planetLinks.length; i++) {
+        let planetLink = planetLinks[i].dataset.url;
+        bindPlanetNameToButton(planetLink, voteButtons[i])
+    }
+}
+
+function bindPlanetNameToButton(link, button) {
+    let planetRequest = new XMLHttpRequest();
+    planetRequest.open('GET', link);
+    planetRequest.onload = function () {
+        let planetData = JSON.parse(planetRequest.responseText);
+        button.setAttribute('data-planet-name', planetData.name);
+        button.setAttribute('data-planet-id', link.slice(29, -1))
+    };
+    planetRequest.send();
+}
+
+function sendPlanetNameToServer() {
+    $(document).ready(function () {
+        let clicked;
+        $(".vote-button").click(function () {
+            clicked = $(this).attr("data-planet-name");
+            $.ajax({
+                type: 'POST',
+                url: "/test",
+                contentType: 'application/json;charset=UTF-8',
+                data: {'data': clicked}
+            });
+        });
+    });
+}
+
+
