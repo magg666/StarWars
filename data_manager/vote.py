f993bedd653e70f23e227748f5187cbfb1a442df
magg666 <mw23127@gmail.com>
2019-04-01 00:44:50 +0200

    added voted logic functions - for voting statistic

--- data_manager/vote.py
+++ data_manager/vote.py
@@ -13,3 +13,11 @@ def get_by_planet(planet_name):
 def vote_planet(planet_data):
     return vote_manager.add_vote(planet_data)
 
+
+def get_for_planets():
+    return vote_manager.count_vote_for_planets()
+
+
+def get_voted_planets_for_user(username):
+    voted_planet = vote_manager.find_voted_planet(username)
+    return list(voted_planet['counted'])
