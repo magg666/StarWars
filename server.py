a0418d12e9ca12e0631bfba58a3c139281f25462
magg666 <mw23127@gmail.com>
2019-04-01 00:58:34 +0200

    deleted secret key, added voting statistic / error logs/ vote routing

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, request, redirect, session, url_for, jsonify
-from functools import wraps
+from flask import Flask, render_template, request, session, jsonify
+import os
 
+import logging_rules as log
 import user as user
 import vote as vote_
 
 app = Flask(__name__)
-app.secret_key = 'uuu'
+app.secret_key = os.environ.get('SECRET_KEY')
 
 
 @app.route('/')
@@ -40,25 +41,26 @@ def logout():
 
 @app.route('/registration', methods=['POST'])
 def registration():
-    username = request.form['login']
-    first_password = request.form['pass']
-    validation_password = request.form['secondpass']
+    data = request.get_json()
+    username = data['username']
+    first_password = data['password']
+    validation_password = data['confirmPassword']
 
     if not user.is_all_data_validate(username, first_password, validation_password):
-        return jsonify({'error': 'Please provide all data'})
+        return jsonify({'state': 'empty'})
 
     if user.exists_already(username):
-        return jsonify({'error': 'This user already exists'})
+        return jsonify({'state': 'in_base'})
 
     if not user.is_passwords_equal(first_password, validation_password):
-        return jsonify({'error': 'Passwords must be equals'})
+        return jsonify({'state': 'not_equal'})
 
     else:
         user.registration(username, first_password)
-        return jsonify({'login': username})
+        return jsonify({'state': 'success'})
 
 
-@app.route('/vote', methods=['GET', 'POST'])
+@app.route('/vote', methods=['POST'])
 def vote():
     planet_data = request.get_json()
     username = planet_data['username']
@@ -70,6 +72,26 @@ def vote():
     if user.is_id_correct(username, user_id):
         planet_name = vote_.vote_planet(planet_vote_data)
         return jsonify({'state': planet_name['planet_name']})
+
+
+@app.route('/user-vote', methods=['POST'])
+def user_vote():
+    data = request.get_json()
+    username = data['username']
+    voted_planets = vote_.get_voted_planets_for_user(username)
+    return jsonify({'planetsId': voted_planets})
+
+
+@app.route('/statistic')
+def statistic():
+    planet_votes = vote_.get_for_planets()
+    return jsonify({'planet_votes': planet_votes})
+
+
+@app.route('/error', methods=['POST'])
+def log_js_errors():
+    error = request.get_json()
+    log.logger.critical('%s', error)
 
 
 if __name__ == '__main__':
