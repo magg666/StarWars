7f5d487e5950e58e2c6c9174e6604ee62dd50308
magg666 <mw23127@gmail.com>
2019-04-01 00:50:55 +0200

    refactored, changed structure

--- static/js/planets.js
+++ static/js/planets.js
@@ -1,77 +1,18 @@
-export {startDisplayingPlanetTable}
-
 import {clearElement} from "./dom_handler.js";
+import {removeCellsWithVoteButtons} from "./vote.js";
 import {prepareResidentsButton} from "./residents.js";
-import {controlVoteButtons, removeCellsWithVoteButtons} from "./vote.js";
+import {controlVoteButtons} from "./vote.js";
+import {formatPlanet} from "./data_formatter.js";
+import {getData, sendDataToSessionStorage} from "./api_ajax.js";
 
-// various parameter for planet table
-
-const starWarsPlanetRequest = new XMLHttpRequest();
-
+export {displayPlanetPage}
 
 const planetParameters = {
     nextButton: document.getElementById('next-button'),
     previousButton: document.getElementById('previous-button'),
+    urlIndex: 7,
+    spinner: document.querySelector('.planet-spinner')
 };
-
-function formatPlanet(planets) {
-        return planets.map(function(planet){
-        let name = planet.name,
-            diameter = planet['diameter'] >= 0 ? planet['diameter'] + ' km' : 'unknown',
-            climate = planet['climate'],
-            terrain = planet['terrain'],
-            surfaceWater = planet['surface_water'] >= 0 ? planet['surface_water'] + ' %' : 'unknown',
-            population = planet['population'] > 0 ? Number(planet['population']).toLocaleString('ja-JP') + ' people' : 'unknown',
-            residents = planet['residents'].length > 0 ?
-            `<button class="residents-button btn btn-outline-info">${planet['residents'].length} ${'resident(s)'} </button>`
-            : '<button class="residents-button hidden"></button><span>No known residents</span>',
-            url = planet.url;
-            return [name, diameter, climate, terrain, surfaceWater, population, residents, url]
-    });
-}
-
-// send request for data (for planet)
-function getDataForPlanets(link){
-    document.querySelector('.spinner-border').innerHTML = "<span style='font-size: 24px'>Loading...</span>";
-    starWarsPlanetRequest.open('GET', link);
-    starWarsPlanetRequest.onreadystatechange = function () {
-        if(starWarsPlanetRequest.readyState === 4 && starWarsPlanetRequest.status === 200){
-            document.querySelector('.spinner-border').innerHTML = "";
-            let starWarsPlanetData = JSON.parse(starWarsPlanetRequest.responseText);
-            makeButtonsWorkingProperly(starWarsPlanetData);
-            createPlanetTable(formatPlanet(starWarsPlanetData.results));
-            prepareResidentsButton();
-            controlVoteButtons();
-
-        }
-    };starWarsPlanetRequest.send();
-}
-
-// functions for buttons previous and next - to navigate between pages with planets' data
-
-function isEndOfPages(resultOfLinkToPage) {
-    return resultOfLinkToPage === null
-}
-
-function addNavButtonFunctions(button, link) {
-
-    if (!isEndOfPages(link)) {
-        button.disabled = false;
-        button.addEventListener("click", function () {
-            getDataForPlanets(link)
-        });
-    } else {
-        button.disabled = true
-    }
-}
-
-function makeButtonsWorkingProperly(planetsData) {
-    let nextPage = planetsData['next'];
-    let previousPage = planetsData['previous'];
-    addNavButtonFunctions(planetParameters.nextButton, nextPage);
-    addNavButtonFunctions(planetParameters.previousButton, previousPage);
-
-}
 
 function createPlanetTable(planetData) {
     clearElement('#table-body');
@@ -79,31 +20,72 @@ function createPlanetTable(planetData) {
     const tableBody = document.querySelector('#table-body');
     let rows = planetData.length;
 
+    for (let k = 0; k < rows; k++) {
+        let onePlanet = planetData[k];
+        let allCells = '';
 
-    for (let i = 0; i < rows; i++) {
-        let onePlanet = planetData[i];
-        let cells = onePlanet.length - 1;
-        let rowId = 'row' + i;
-        let tableRow = document.createElement('tr');
-        tableRow.classList.add('row-class');
-        tableRow.setAttribute('id', rowId);
-        tableRow.setAttribute('data-url', onePlanet[7]);
-        tableBody.appendChild(tableRow);
-
-        for (let j = 0; j < cells; j++) {
-            const tableCell = document.createElement('td');
-            tableCell.classList.add('cell' + j);
-            tableCell.innerHTML = onePlanet[j];
-            tableRow.appendChild(tableCell)
+        for (let j = 0; j < onePlanet.length - 1; j++) {
+            let cell = `
+            <td class="cell${j}">${onePlanet[j]}</td>`;
+            allCells += cell
         }
+
+        let row = `
+        <tr class="row-class" id="row${k}" data-url="${onePlanet[planetParameters.urlIndex]}">${allCells}</tr>`;
+        tableBody.insertAdjacentHTML("beforeend", row);
     }
 }
 
+function isEndOfPages(resultOfLinkToPage) {
+    return resultOfLinkToPage === null;
+}
 
-function startDisplayingPlanetTable(link) {
-    if(link === undefined){
+function addFunctionalityToPaginationButton(button, link) {
+    if (!isEndOfPages(link)) {
+        button.disabled = false;
+        button.addEventListener('click', changePage)
+    }
+    else {
+        button.disabled = true
+    }
+}
+
+function preparePaginationButtons(planetsData) {
+    let nextPage = planetsData['next'];
+    let previousPage = planetsData['previous'];
+    addFunctionalityToPaginationButton(planetParameters.nextButton, nextPage);
+    addFunctionalityToPaginationButton(planetParameters.previousButton, previousPage);
+    planetParameters.nextButton.setAttribute('data-url', nextPage);
+    planetParameters.previousButton.setAttribute('data-url', previousPage);
+}
+
+function changePage(event) {
+    let link = event.target.getAttribute('data-url');
+    getData(link, planetParameters.spinner, preparePlanetPage.bind(null, link))
+}
+
+function checkSessionStorageForPlanetData(link, data) {
+    let formattedData = {};
+    if(!sessionStorage.getItem(link)){
+        formattedData = formatPlanet(data);
+    }else{
+        formattedData = data
+    }return formattedData
+}
+
+function preparePlanetPage(link, data) {
+    let formattedData = checkSessionStorageForPlanetData(link, data);
+    sendDataToSessionStorage(link, formattedData);
+    createPlanetTable(formattedData.planet);
+    preparePaginationButtons(formattedData);
+    prepareResidentsButton();
+    controlVoteButtons();
+}
+
+function displayPlanetPage(link) {
+    if (link === undefined) {
         link = 'https://swapi.co/api/planets/?page=1';
     }
-    getDataForPlanets(link);
-
+    getData(link, planetParameters.spinner, preparePlanetPage.bind(null, link));
 }
+
